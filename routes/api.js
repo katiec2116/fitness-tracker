9b9010f30f2d16b85209b2912e8a5c35b3e51cef
katiec <katiecampbell21@outlook.com>
2020-10-30 23:01:45 -0400

    edit routes

--- routes/api.js
+++ routes/api.js
@@ -1,64 +1,54 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
-
+const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-        .then(results => {
-            res.json(results);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-router.get("/api/workouts", (req, res) => {
-    Workout.find()
-        .then(results => {
-            res.json(results);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-})
-
-router.put("/api/workouts/:id", (req, res) => {
-    const exercise = req.body
-    Workout.findByIdAndUpdate(
-        req.params.id, 
-    {
-        $push: {exercises: exercise}
-    },{
-        new: true, runValidators: true
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
-        .then(results => {
-            res.json(results);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-})
-
-
-
+    .catch(err => {
+      res.json(err);
+    });
+});
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find({}).limit(7)
-        .then(results => {
-            res.json(results);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-})
-
-router.delete("/api/workouts", (req, res) => {
-    Workout.findByIdAndDelete(req.body.id)
-      .then(() => {
-        res.json(true);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-module.exports = router
+  Workout.find({}).limit(7)
+    .then(dbWorkouts => {
+      console.log(dbWorkouts)
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+module.exports = router;
